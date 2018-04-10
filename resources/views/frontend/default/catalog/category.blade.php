b8feee83ce9a775f8102b4da15ecba22368f58f5
wangyue <18510209803@163.com>
2018-04-11 00:36:36 +1000

    Update

--- resources/views/frontend/default/catalog/category.blade.php
+++ resources/views/frontend/default/catalog/category.blade.php
@@ -13,9 +13,15 @@
                 <div class="columns">
                     @foreach($featureProducts as $featureProduct)
                     <div class="column">
-                        <a href="{{ url('catalog/product/'.$featureProduct->uri) }}">
+                        <div class="show-mask-on-hover">
                             <img src="{{ $featureProduct->getProductDefaultImageUrl() }}" alt="{{ $featureProduct->name }}" class="image mb-10">
-                        </a>
+                            <div class="mask">
+                                <a href="{{ url('catalog/product/'.$featureProduct->uri) }}">
+                                    <p class="name is-size-4">{{ $featureProduct->name }}</p>
+                                    <p class="price is-size-5">${{ $featureProduct->getFinalPriceGst() }}</p>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                     @endforeach
                 </div>
