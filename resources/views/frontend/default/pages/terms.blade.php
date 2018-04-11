20899386623915ce55153e61d697a9758a2401c5
Justin Wang <justinwang24@yahoo.com.au>
2018-04-11 15:58:09 +1000

    Update

--- resources/views/frontend/default/pages/terms.blade.php
+++ resources/views/frontend/default/pages/terms.blade.php
@@ -1,6 +1,11 @@
 @extends(_get_frontend_layout_path('frontend'))
 @section('content')
     <div class="content pt-40">
+        <div class="page-title-wrap">
+            <h1 class="is-size-1-desktop is-size-1-mobile" style="margin-top:0px; padding-top: 20px;">
+                {!! 'cn'==app()->getLocale() ? $page->title_cn : $page->title !!}
+            </h1>
+        </div>
         {!! $page ? $page->rebuildContent() : null !!}
     </div>
 @endsection
