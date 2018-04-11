5ae1f062e92431e08faff708315c7fd3c272cefd
Justin Wang <justinwang24@yahoo.com.au>
2018-04-11 16:02:54 +1000

    Update

--- resources/views/frontend/default/templates/one_column.blade.php
+++ resources/views/frontend/default/templates/one_column.blade.php
@@ -1,10 +1,17 @@
 @extends(_get_frontend_layout_path('frontend'))
 @section('content')
-
-<div class="content pl-20 pr-20 page-content-wrap mb-20">
-        <div class="page-title-wrap">
-        <h1 class="is-size-1-desktop is-size-1-mobile" style="margin-top:0px; padding-top: 20px;">{!! 'cn'==app()->getLocale() ? $page->title_cn : $page->title !!}</h1>
+<div class="columns">
+        <div class="column is-1"></div>
+        <div class="column">
+                <div class="content pt-40">
+                        <div class="page-title-wrap">
+                                <h1 class="is-size-1-desktop is-size-1-mobile" style="margin-top:0px; padding-top: 20px;">
+                                        {!! 'cn'==app()->getLocale() ? $page->title_cn : $page->title !!}
+                                </h1>
+                        </div>
+                        {!! $page->rebuildContent() !!}
+                </div>
         </div>
-        {!! $page->rebuildContent() !!}
+        <div class="column is-1"></div>
 </div>
 @endsection
