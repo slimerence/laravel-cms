cc644c877d2a0735c9b525bd30012f16f187396b
slimerence <39113678+slimerence@users.noreply.github.com>
2018-06-21 11:10:22 +1000

    为了Maydriver导航作出的路由修正

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', 'Frontend\Pages@index')->name('home');
 Route::get('/contact-us', 'Frontend\Pages@contact_us')->name('contact_us');
 Route::post('/contact-us', 'Frontend\Pages@contact_us_handler');
 Route::get('/terms', 'Frontend\Pages@terms')->name('terms');
-
+Route::get('/results', 'Frontend\Pages@news');
 // 特定的URI
 Route::prefix('page')->group(function(){
     Route::get('/blog', 'Frontend\Pages@blog');
